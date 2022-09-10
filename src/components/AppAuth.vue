effc32c689787610089484c514f711cafc590a0c
Pavel <sculltakers111@gmail.com>
2022-09-10 18:23:35 +0300

    added custom validation messages for register form

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -81,7 +81,7 @@
           </form>
           <!-- Registration Form -->
           <vee-form v-show="tab === 'register'" :validation-schema="schema"
-            @submit="register">
+            @submit="register" :initial-values="userData">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -112,12 +112,13 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field name="password"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-              <ErrorMessage class="text-red-600" name="password" />
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                       placeholder="Password" type="password" v-bind="field" />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -149,7 +150,7 @@
                 class="w-4 h-4 float-left -ml-6 mt-1 rounded"
               />
               <label class="inline-block">Accept terms of service</label>
-              <ErrorMessage class="text-red-600" name="tos" />
+              <ErrorMessage class="text-red-600 block" name="tos" />
             </div>
             <button
               type="submit"
@@ -177,10 +178,13 @@ export default {
         name: "required|min:3|max:100|alpha_spaces",
         email: "required|min:3|max:100|email",
         age: "required|min_value:18|max_value:100",
-        password: "required|min:3|max:100",
-        confirm_password: "confirmed:@password",
-        country: "required|excluded:Antarctica",
+        password: "required|min:9|max:100|excluded:password",
+        confirm_password: "password_mismatch:@password",
+        country: "required|country_excluded:Antarctica",
         tos: "required"
+      },
+      userData: {
+        country: 'USA'
       }
     }
   },
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,4 +1,4 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from "vee-validate";
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure } from "vee-validate";
 import {
     required, min, max, alpha_spaces as alphaSpaces, email,
     min_value as minVal, max_value as maxVal, confirmed, not_one_of as excluded
@@ -11,13 +11,38 @@ export default {
         app.component("ErrorMessage", ErrorMessage);
 
         defineRule('required', required);
+        defineRule('tos', required);
         defineRule('min', min);
         defineRule('max', max);
         defineRule('alpha_spaces', alphaSpaces);
         defineRule('email', email);
         defineRule('min_value', minVal);
         defineRule('max_value', maxVal);
-        defineRule('confirmed', confirmed);
+        defineRule('password_mismatch', confirmed);
         defineRule('excluded', excluded);
+        defineRule('country_excluded', excluded);
+
+        configure({
+            generateMessage: (ctx) => {
+                const messages = {
+                    required: `The field ${ctx.field} is required.`,
+                    min: `The field ${ctx.field} is too short.`,
+                    max: `The field ${ctx.field} is too long.`,
+                    alpha_spaces: `The field ${ctx.field} may only contain alphabetic chars & spaces.`,
+                    email: `The field ${ctx.field} must be a valid email.`,
+                    min_value: `The field ${ctx.field} is too low.`,
+                    max_value: `The field ${ctx.field} is too high.`,
+                    confirmed: `The field ${ctx.field} is too high.`,
+                    excluded: `You are not allowed to use this field - ${ctx.field}.`,
+                    country_excluded: `We do not accept users from this location.`,
+                    password_mismatch: `The passwrod dont match.`,
+                    tos: `You must accept the Terms of Service.`,
+                };
+                const message = messages[ctx.rule.name]
+                    ? messages[ctx.rule.name]
+                    : `The ${ctx.field} is invalid.`;
+                return message;
+            }
+        });
     }
 }
