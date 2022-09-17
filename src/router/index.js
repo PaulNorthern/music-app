031d9b36b275c161703194bbd6d11e493df44148
Pavel <sculltakers111@gmail.com>
2022-09-17 16:46:54 +0300

    reduce bundle size, use chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import About from "@/views/About.vue";
-import Home from "@/views/Home.vue";
-import Manage from "@/views/Manage.vue";
 import useUserStore from "@/stores/user";
-import Song from "@/views/Song.vue";
+
+// load the component only when they are needed
+const Home = () => import("@/views/Home.vue");
+const About = () => import("@/views/About.vue");
+const Manage = () => import("@/views/Manage.vue");
+const Song = () => import("@/views/Song.vue");
+
 
 const routes = [
   {
