9e238814dbc0ce9a8ffcba51a6a60c57c6fb66e3
Pavel <sculltakers111@gmail.com>
2022-09-19 15:09:34 +0300

    testing attributes

--- src/components/__tests__/songitem.spec.js
+++ src/components/__tests__/songitem.spec.js
@@ -22,4 +22,33 @@ describe('AppSongItem.vue', () => {
 
         expect(compositionAuthor.text()).toBe(song.display_name);
     });
+    test('renders song.docID in id attribute', () => {
+        const song = {
+            docID: 'abc',
+        };
+
+        const wrapper = shallowMount(AppSongItem, {
+            props: {
+                song
+            },
+            global: {
+                components: {
+                    'router-link': RouterLinkStub
+                }
+            }
+        });
+
+        const compositionAuthor = wrapper.find('.text-gray-500');
+
+        // expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`);
+        expect(wrapper.classes()).toContain(`song-id-${song.docID}`);
+    });
 });
+
+
+
+
+
+
+
+
